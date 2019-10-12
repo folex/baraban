4049c0930fe62f67af89f694b00e862b2e9b4a31
folex <0xdxdy@gmail.com>
2019-10-12 21:31:43 +0300

    send & receive heartbeat

--- app/Main.hs
+++ app/Main.hs
@@ -4,12 +4,13 @@
 module Main where
 
 import Lib
-import Control.Concurrent (forkFinally, forkIO)
+import Control.Concurrent (forkFinally, forkIO, threadDelay)
 import Control.Monad (forever, unless, void)
 import Data.Maybe (fromJust)
-import Network.Socket (Socket)
+import Network.Socket hiding (recv, recvFrom, send, sendTo)
 import Network.Socket.ByteString
 import System.Environment (getArgs)
+import Data.ProtoLens.Encoding (encodeMessage)
 import qualified Control.Exception as E
 import qualified Data.ByteString as S
 
@@ -19,14 +20,31 @@ main = do
   (port, host) <- getPortHost $ tail args
   case head args of
     "listen" -> listenAt port host receive
-    "connect" -> connectTo port $ fromJust host
+    "connect" -> connectTo (fromJust host) port
 
 receive :: Socket -> IO ()
-receive conn = do
+receive conn = forever $ do
   msg <- recv conn 1024
   unless (S.null msg) $ do
     let raft = decodeRaftMessage msg
     putStrLn $ "Got message " ++ show raft
 
 connectTo :: String -> String -> IO ()
-connectTo port host = putStrLn $ "TODO: Will connect to " ++ host ++ ":" ++ port
+connectTo host port = runTCPClient host port talk
+  where
+    talk conn = repeatedly $ sendAll conn $ encodeMessage $ heartbeat 1000
+    repeatedly action = forever $ threadDelay (1 * 1000000) >> action
+
+
+runTCPClient :: HostName -> ServiceName -> (Socket -> IO ()) -> IO ()
+runTCPClient host port client = withSocketsDo $ do
+    addr <- resolve
+    E.bracket (open addr) close (\s -> (client s >> putStrLn "closed bracket"))
+  where
+    resolve = do
+        let hints = defaultHints { addrSocketType = Stream }
+        head <$> getAddrInfo (Just hints) (Just host) (Just port)
+    open addr = do
+        sock <- socket (addrFamily addr) (addrSocketType addr) (addrProtocol addr)
+        connect sock $ addrAddress addr
+        return sock
